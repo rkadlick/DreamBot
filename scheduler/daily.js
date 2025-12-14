0946ffbb4d46f72ec1f1fb22c91733110bb643e7
rkadlick <27776470+rkadlick@users.noreply.github.com>
2025-12-14 12:31:49 -0500

    fix: pause trivia component

--- scheduler/daily.js
+++ scheduler/daily.js
@@ -1,21 +1,25 @@
+require('dotenv').config();
 const cron = require('node-cron');
-const { clearGamesChannel } = require('../games/clearGamesChannel.js');
+// const { clearGamesChannel } = require('../games/clearGamesChannel.js');
 const { addQuestion } = require('../trivia/addQuestion.js');
 const { postQuestion } = require('../trivia/postQuestion.js');
 
 function daily(client) {
-	// 5am 
-	cron.schedule('00 05 * * *', () => {
-		clearGamesChannel(client);
-	});
-	// 10:50am
-	cron.schedule('50 10 * * *', () => {
-		addQuestion();
-	});
-	// 11am
-	cron.schedule('00 11 * * *', () => {
-		postQuestion(client);
-	});
+	// 5am
+	// cron.schedule('00 05 * * *', () => {
+	// 	clearGamesChannel(client);
+	// });
+	// Trivia scheduling guarded by TRIVIA_ENABLED
+	if (process.env.TRIVIA_ENABLED === 'true') {
+		// 10:50am
+		cron.schedule('50 10 * * *', () => {
+			addQuestion();
+		});
+		// 11am
+		cron.schedule('00 11 * * *', () => {
+			postQuestion(client);
+		});
+	}
 }
 
 module.exports = { daily };
